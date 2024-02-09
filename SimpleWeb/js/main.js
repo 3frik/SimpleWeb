39052fa52d9f9f36c90cbd75655c6f3b85c76128
3frik <86358988+3frik@users.noreply.github.com>
2024-02-09 13:23:14 +0100

    First upload

---
+++ SimpleWeb/js/main.js
@@ -0,0 +1,29 @@
+var savedStuff;
+
+function button1Click(){    //Alert
+    alert("Button 1!");
+}
+
+function button2Click(){    //Hide
+}
+
+function button3Click(){    //src
+}
+
+function button4Click(){    //Save in variable
+}
+
+function button5Click(){    //innerText
+}
+
+function button6Click(){    //several things
+}
+
+function button7Click(){    //innerHTML
+}
+
+function button8Click(){    
+}
+
+function button9Click(){
+}
